2e6ea3b7b52fde340cc4dc61daf2428dbba33893
takayums <takayums20@gmail.com>
2024-05-26 07:13:39 +0700

    feat(home): add section featured, newslatter, and browse products

--- src/assets/index.jsx
+++ src/assets/index.jsx
@@ -1,5 +1,6 @@
 import Logo from "./logo.svg";
 import User from "./user.png";
 import Tshirt from "./tshirt.png";
+import Tshirt2 from "./tshirt2.png";
 
-export { Logo, User, Tshirt };
+export { Logo, User, Tshirt, Tshirt2 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import { BiRightArrowAlt } from "react-icons/bi";
 import { TbTruckDelivery } from "react-icons/tb";
 import { BsStars } from "react-icons/bs";
 import { RiSecurePaymentLine } from "react-icons/ri";
-import { Logo, Tshirt, User } from "../assets";
+import { Tshirt, Tshirt2, User } from "../assets";
 
 const Home = () => {
   return (
@@ -19,7 +19,7 @@ const Home = () => {
             </p>
             <button className="px-6 py-3 flex items-center bg-black font-medium text-white rounded-md">
               View Collection
-              <span className="p-2">
+              <span className="p-1">
                 <BiRightArrowAlt className="text-xl" />
               </span>
             </button>
@@ -143,6 +143,134 @@ const Home = () => {
                 <span className="text-base text-gray-600">$35.00</span>
               </div>
             </div>
+          </div>
+        </div>
+
+        <div className="grid grid-cols-2 items-center h-[440px]">
+          <div className="">
+            <h3 className="text-2xl font-semibold text-black mb-3">
+              Browse Our Fashion Paradise!
+            </h3>
+            <p className="text-sm text-gray-500 mb-12">
+              Step into a world of style and explore our diverse collection of
+              clothing categories.
+            </p>
+            <button className="px-6 py-3 flex items-center bg-black font-medium text-white rounded-md">
+              Sart Browsing
+              <span className="p-1">
+                <BiRightArrowAlt className="text-2xl" />
+              </span>
+            </button>
+          </div>
+          <div className="w-[225px] ml-auto">
+            <img src={Tshirt2} alt="User" />
+          </div>
+        </div>
+
+        <div className="mb-20">
+          <div className="text-center mb-12">
+            <span className="px-4 py-1 mr-4 rounded-full border border-gray-300 font-medium text-sm uppercase">
+              Features
+            </span>
+            <span className="px-4 py-1 mr-4 rounded-full text-sm uppercase text-gray-500 font-medium">
+              Latest
+            </span>
+          </div>
+          <div className="grid grid-cols-4 gap-5">
+            <div className="py-4 px-2">
+              <div className="w-full rounded bg-gray-200 mb-6">
+                <img
+                  src={Tshirt}
+                  alt="Tshirt"
+                  className="w-full object-cover object-center"
+                />
+              </div>
+              <div>
+                <p className="text-sm font-medium mb-3">
+                  Classic Monochrome Tees
+                </p>
+                <span className="px-4 py-1 mr-4 rounded-full border border-gray-300 text-sm uppercase">
+                  In Stock
+                </span>
+                <span className="text-base text-gray-600">$35.00</span>
+              </div>
+            </div>
+            <div className="py-4 px-2">
+              <div className="w-full rounded bg-gray-200 mb-6">
+                <img
+                  src={Tshirt}
+                  alt="Tshirt"
+                  className="w-full object-cover object-center"
+                />
+              </div>
+              <div>
+                <p className="text-sm font-medium mb-3">
+                  Classic Monochrome Tees
+                </p>
+                <span className="px-4 py-1 mr-4 rounded-full border border-gray-300 text-sm uppercase">
+                  In Stock
+                </span>
+                <span className="text-base text-gray-600">$35.00</span>
+              </div>
+            </div>
+            <div className="py-4 px-2">
+              <div className="w-full rounded bg-gray-200 mb-6">
+                <img
+                  src={Tshirt}
+                  alt="Tshirt"
+                  className="w-full object-cover object-center"
+                />
+              </div>
+              <div>
+                <p className="text-sm font-medium mb-3">
+                  Classic Monochrome Tees
+                </p>
+                <span className="px-4 py-1 mr-4 rounded-full border border-gray-300 text-sm uppercase">
+                  In Stock
+                </span>
+                <span className="text-base text-gray-600">$35.00</span>
+              </div>
+            </div>
+            <div className="py-4 px-2">
+              <div className="w-full rounded bg-gray-200 mb-6">
+                <img
+                  src={Tshirt}
+                  alt="Tshirt"
+                  className="w-full object-cover object-center"
+                />
+              </div>
+              <div>
+                <p className="text-sm font-medium mb-3">
+                  Classic Monochrome Tees
+                </p>
+                <span className="px-4 py-1 mr-4 rounded-full border border-gray-300 text-sm uppercase">
+                  In Stock
+                </span>
+                <span className="text-base text-gray-600">$35.00</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="bg-gray-300">
+        <div className="container mx-auto max-w-[1116px] flex items-center justify-between p-14">
+          <div className="basis-1/2">
+            <h3 className="text-2xl capitalize font-bold mb-6">
+              Joint Our Newslatter
+            </h3>
+            <p className="font-normal text-sm text-gray-500">
+              We love to surprise our subscribers with occasional gifts.
+            </p>
+          </div>
+          <div>
+            <input
+              type="email"
+              placeholder="Your email address"
+              className="px-4 py-3 rounded-md text-sm mr-4"
+            />
+            <button className="text-sm font-medium text-white px-4 py-3 bg-black rounded-md">
+              Subscribe
+            </button>
           </div>
         </div>
       </div>
