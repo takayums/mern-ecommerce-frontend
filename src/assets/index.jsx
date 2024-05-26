ce9488cf24ed17f4aab7767621b9baed705585de
takayums <takayums20@gmail.com>
2024-05-26 08:06:38 +0700

    feat(footer): add footer

--- src/assets/index.jsx
+++ src/assets/index.jsx
@@ -2,5 +2,8 @@ import Logo from "./logo.svg";
 import User from "./user.png";
 import Tshirt from "./tshirt.png";
 import Tshirt2 from "./tshirt2.png";
+import Visa from "./visa.png";
+import Mastercard from "./mastercard.png";
+import Amex from "./amex.png";
 
-export { Logo, User, Tshirt, Tshirt2 };
+export { Logo, User, Tshirt, Tshirt2, Amex, Mastercard, Visa };
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,98 @@
 import React from "react";
+import { Amex, Logo, Mastercard, Visa } from "../assets";
+import { FaGithub, FaInstagram, FaYoutube } from "react-icons/fa";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="mt-10">
+      <div className="container max-w-[1116px] mx-auto">
+        <div className="flex justify-between mb-20">
+          <div className="w-[272px]">
+            <div className="flex items-center gap-4 mb-3">
+              <img src={Logo} className="bg-black px-3 py-1 rounded-lg" />
+              <p className="font-bold text-xl">Ecommerce</p>
+            </div>
+            <p className="text-gray-500 text-sm mb-6">
+              DevCut is a YouTube channel for practical project-based learning.
+            </p>
+            <div className="flex text-2xl gap-6 items-center">
+              <FaGithub />
+              <FaInstagram />
+              <FaYoutube />
+            </div>
+          </div>
+
+          <div className="grid grid-cols-3 gap-6">
+            <div>
+              <p className="mb-10 text-sm font-medium text-gray-400 uppercase">
+                support
+              </p>
+              <ul className="space-y-4">
+                <li className="text-sm text-gray-700 font-medium capitalize">
+                  FAQ
+                </li>
+                <li className="text-sm text-gray-700 font-medium capitalize">
+                  terms of you
+                </li>
+                <li className="text-sm text-gray-700 font-medium capitalize">
+                  pricacy policy
+                </li>
+              </ul>
+            </div>
+            <div>
+              <p className="mb-10 text-sm font-medium text-gray-400 uppercase">
+                company
+              </p>
+              <ul className="space-y-4">
+                <li className="text-sm text-gray-700 font-medium capitalize">
+                  About Us
+                </li>
+                <li className="text-sm text-gray-700 font-medium capitalize">
+                  Contacts
+                </li>
+                <li className="text-sm text-gray-700 font-medium capitalize">
+                  Careers
+                </li>
+              </ul>
+            </div>
+            <div>
+              <p className="mb-10 text-sm font-medium text-gray-400 uppercase">
+                shop
+              </p>
+              <ul className="space-y-4">
+                <li className="text-sm text-gray-700 font-medium capitalize">
+                  my account
+                </li>
+                <li className="text-sm text-gray-700 font-medium capitalize">
+                  checkout
+                </li>
+                <li className="text-sm text-gray-700 font-medium capitalize">
+                  cart
+                </li>
+              </ul>
+            </div>
+          </div>
+
+          <div>
+            <p className="mb-10 text-sm font-medium text-gray-400 uppercase">
+              accepted payment
+            </p>
+            <div className="flex gap-4">
+              <img src={Mastercard} alt="Mastercard" className="grayscale" />
+              <img src={Amex} alt="Amex" className="grayscale" />
+              <img src={Visa} alt="Visa" className="grayscale" />
+            </div>
+          </div>
+        </div>
+
+        <div className="text-center p-6">
+          <p className="text-sm text-gray-500">
+            &copy; 2024 DevCut. All rights reserved.
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
